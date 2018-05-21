6e7168509314f31656bd7442058d8d08ee2d84bb
Gudrun Socher <gudrun.socher@hm.edu>
2018-05-21 16:16:03 +0200

    textures branch

--- shader/cg/FragmentShader.glsl
+++ shader/cg/FragmentShader.glsl
@@ -20,10 +20,7 @@ struct Light
 
 struct FragmentInput
 {
-    vec3 normal;
-
-	vec3 lightDir;
-	vec3 viewDir;
+	vec2 uv;
 };
 
 struct Normalized
@@ -31,6 +28,11 @@ struct Normalized
     vec3 normal;
     vec3 lightDir;
     vec3 viewDir;
+};
+
+struct Textures
+{
+	sampler2D tex;
 };
 // =============================================================================================================
 
@@ -47,11 +49,15 @@ subroutine void FragmentProgram();
 
 // Variables
 // =============================================================================================================
-layout(location = 0) flat in FragmentInput Input;
+layout(location = 0) in FragmentInput Input;
 
 uniform Matrices matrices;
 
 uniform Light light;
+
+uniform Textures textures;
+
+uniform float animStage;
 
 subroutine uniform FragmentProgram fragmentprogram;
 
@@ -74,44 +80,8 @@ void main()
 
 // Subroutine Implementation
 // =============================================================================================================
-layout (index = 0) subroutine (FragmentProgram) void toon()
+layout (index = 0) subroutine (FragmentProgram) void textured()
 {
-    Normalized n;
-    n.viewDir = normalize(Input.viewDir);
-    n.lightDir = normalize(Input.lightDir);
-    n.normal = normalize(Input.normal);
-
-    float intensity = dot(n.lightDir, n.normal);
-
-    vec4 toon = vec4(1.f);
-
-    if(abs(intensity) > .9f)
-        toon = vec4(1.f);
-
-    else if(abs(intensity) > .8f)
-        toon = vec4(.8f);
-
-    else if(abs(intensity) > .7f)
-        toon = vec4(.7f);
-
-    else if(abs(intensity) > .6f)
-        toon = vec4(.6f);
-
-    else if(abs(intensity) > .5f)
-        toon = vec4(.5f);
-
-    else if(intensity > .4f)
-        toon = vec4(.4f);
-
-    else if(intensity > .3f)
-        toon = vec4(.3f);
-
-    else
-        toon = vec4(.2f);
-
-    toon.w = 1.f;
-
-
-    out_color = toon * vec4((Input.normal * .5f) + .5f, 1);
+	out_color = texture(textures.tex, Input.uv);
 }
 // =============================================================================================================
